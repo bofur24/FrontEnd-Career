b5441ac4c309a792cfc9232c96c7070ebaf1f67e
bofur24 <dransiktolkien@yahoo.com>
2020-10-22 15:05:26 -0500

    Project-2

---
+++ Module-1/Project-2/webpack.config.js
@@ -0,0 +1,25 @@
+export default {
+  "output": {
+    "filename": "[name].pack.js"
+  },
+  "module": {
+    "rules": [
+      {
+        "use": {
+          "loader": "babel-loader",
+          "options": {
+            "presets": [
+              "babel-preset-env",
+              "babel-preset-react"
+            ]
+          }
+        },
+        "exclude": /node_modules/,
+        "test": /\.js$/
+      }
+    ]
+  },
+  "entry": {
+    "index": "./index"
+  }
+};
