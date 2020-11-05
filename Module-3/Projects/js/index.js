c4c1db05a05bb7ba670ca3eaef9a6b73fe313d44
bofur24 <dransiktolkien@yahoo.com>
2020-11-05 07:56:53 -0600

    Changing a website with javaScript

--- Module-3/Projects/js/index.js
+++ Module-3/Projects/js/index.js
@@ -4,3 +4,10 @@ var studentGreeting = "Hello my name is Keith Heffington! I'm a Scrimba student"
 
 console.log(teacherGreeting);
 console.log(studentGreeting);
+
+var awesomeHeader = document.getElementById("myHeader")
+console.log(awesomeHeader)
+awesomeHeader.style.background = 'red';
+awesomeHeader.style.color = 'green';
+
+
