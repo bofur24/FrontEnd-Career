d8c6cf977833894b4338da1bb44e04a7a0fe0e63
bofur24 <dransiktolkien@yahoo.com>
2020-11-05 07:46:16 -0600

    My First Javascript

---
+++ Module-3/Projects/js/index.js
@@ -0,0 +1,6 @@
+
+var teacherGreeting = "Hello from Per, your teacher!";
+var studentGreeting = "Hello my name is Keith Heffington! I'm a Scrimba student"
+
+console.log(teacherGreeting);
+console.log(studentGreeting);
