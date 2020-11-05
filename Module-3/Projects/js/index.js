a99bef9ccec44e52a810c6e539797205c253b7c3
bofur24 <dransiktolkien@yahoo.com>
2020-11-05 08:14:31 -0600

    Adding interactivity to the button

--- Module-3/Projects/js/index.js
+++ Module-3/Projects/js/index.js
@@ -1,16 +1,19 @@
 
-var teacherGreeting = "Hello from Per, your teacher!";
-var studentGreeting = "Hello my name is Keith Heffington! I'm a Scrimba student"
+// var teacherGreeting = "Hello from Per, your teacher!";
+// var studentGreeting = "Hello my name is Keith Heffington! I'm a Scrimba student"
 
-console.log(teacherGreeting);
-console.log(studentGreeting);
+// console.log(teacherGreeting);
+// console.log(studentGreeting);
 
-var awesomeHeader = document.getElementById("myHeader")
-console.log(awesomeHeader)
-awesomeHeader.style.background = 'red';
-awesomeHeader.style.color = 'green';
+// var awesomeHeader = document.getElementById("myHeader")
+// console.log(awesomeHeader)
+// awesomeHeader.style.background = 'red';
+// awesomeHeader.style.color = 'green';
 
 var myButton = document.getElementById("clickMeButton");
 console.log(myButton)
 
-
+myButton.addEventListener("click", function(){
+    // console.log("button was clicked")
+    myButton.style.background = "red"
+})
