d9921164e1e790453c82a328e7f072b7cbec3491
bofur24 <dransiktolkien@yahoo.com>
2020-11-05 08:45:50 -0600

    Created a counter app

--- Module-3/Projects/js/index.js
+++ Module-3/Projects/js/index.js
@@ -10,14 +10,25 @@
 // awesomeHeader.style.background = 'red';
 // awesomeHeader.style.color = 'green';
 
+// var myButton = document.getElementById("clickMeButton")
+// console.log(myButton)
+// var myTitle = document.getElementById("title")
+// var counter = 0
+// myButton.addEventListener("click", function(){
+//     // console.log("button was clicked")
+//     // myButton.style.background = "red"
+//     // myTitle.textContent = "I'm learning JavaScript"
+//     counter = 2
+//     myButton.textContent = counter
+// })
+
+
 var myButton = document.getElementById("clickMeButton")
-console.log(myButton)
-var myTitle = document.getElementById("title")
-var counter = 0
+var counter = 5
+
 myButton.addEventListener("click", function(){
-    // console.log("button was clicked")
-    // myButton.style.background = "red"
-    // myTitle.textContent = "I'm learning JavaScript"
-    counter = 2
+
+    counter += 5
+
     myButton.textContent = counter
 })
