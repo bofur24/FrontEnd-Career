c6c15df5c9e7e70b84c26d3697a840c3f67ce95e
bofur24 <dransiktolkien@yahoo.com>
2020-11-10 15:24:05 -0600

    Functions

---
+++ Module-3/Projects/js/Functions.js
@@ -0,0 +1,20 @@
+console.log("Hello and Welcome")
+
+//Write a function that sets off an alert in your console
+//camelCase
+
+//without parameters
+function showAlert() {
+    console.log('You have been alerted')
+}
+
+showAlert()
+
+//with parameters
+
+function showAlert1(firstName, lastName) {
+    console.log(firstName + ' ' + lastName + ', you have been alerted')
+}
+
+showAlert1('Keith', 'Heffington')
+showAlert1('Logan', 'Tom', 'Jenny')
