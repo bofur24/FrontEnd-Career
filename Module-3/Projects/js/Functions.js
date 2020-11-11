76b3b91df6d3e4701e5dc2275747413b5079d33e
bofur24 <dransiktolkien@yahoo.com>
2020-11-11 15:40:49 -0600

    Slice out of an array

--- Module-3/Projects/js/Functions.js
+++ Module-3/Projects/js/Functions.js
@@ -68,3 +68,20 @@ let randomItem = shopItems[nextNumber]
 console.log(randomItem)
 
 
+// slice () method it does NOT mututat arrays it produces new ones.
+
+const films = ['Elf', 'Borat', 'It', 'Split', 'Us']
+
+//get the last 2 films from the array and put them in the thriller section
+const thriller = films.slice(3, 5)
+console.log(thriller)
+console.log(films)
+
+//get the 3rd and 4th film from the array and put them in the horror section
+const horror = films.slice(2, 4)
+console.log(horror)
+
+//get the first 2 films from the array and put them in the comedy section
+
+const comedy = films.slice(0, 2)
+console.log(comedy)
